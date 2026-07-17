7c104973e654f36fdb4baa2a0d734cf4b05b7640
MuhammadAnas-25i3062 <i253062@isb.nu.edu.pk>
2026-07-17 16:38:12 +0500

    removed default things

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//Starting point of the application
+import { StrictMode } from 'react'//An extra debugger for developer
+import { createRoot } from 'react-dom/client'//A place for react to render the application
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(//Render the application in the root element of the HTML
   <StrictMode>
     <App />
   </StrictMode>,
