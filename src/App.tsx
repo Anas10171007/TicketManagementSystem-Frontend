6037b25ebfbf0e734bb910100e8908428d68e305
MuhammadAnas-25i3062 <i253062@isb.nu.edu.pk>
2026-07-19 17:20:37 +0500

    Created app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import AppRouter from "./routes/AppRouter";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-cyan-400">
-        Ticket Management System
-      </h1>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
